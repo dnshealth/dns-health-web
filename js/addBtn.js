15e5f1bddf1ba483ee44d72adc7bee9582e81d83
Kasper Oikarinen <kasper@sourceress.net>
2020-04-28 15:10:45 +0200

    Index page improvements

--- js/addBtn.js
+++ js/addBtn.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         if(counter < max_fields_limit){ //check conditions
             counter++; //counter increment
             $('.input_fields_container_part').append('<div>' +
-                '<input type="text" name="tags" class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"/>' +
+                '<input type="text" name="ns[]" class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"/>' +
                 '<a href="#" class="remove_field" style="margin-left:10px;">Remove</a>' +
                 '</div>'); //add input field
         }
