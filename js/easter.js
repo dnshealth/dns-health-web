1b90b5ef44b698adbd382c7489004b162de49538
Rudolfs Arvids Kalnins <rudolfsarvidskalnins@gmail.com>
2020-05-12 10:58:34 +0200

    added some comments to easter egg javascript

--- js/easter.js
+++ js/easter.js
@@ -1,10 +1,24 @@
+/** 
+* @author Rūdolfs Arvīds Kalniņš
+*
+*/
+'use strict';
+
+// When page has loaded
 $(document).ready(function(){
 
+    // Initialize button states as false (not pressed)
     let counter1 = false;
     let counter2 = false;
     let counter3 = false;
     let counter4 = false;
 
+    /*
+     If button with class ".secret_meme1" is clicked its
+     state "counter1" is set to true (pressed). Then if all other
+     buttons have been pressed, confetti is displayed. This functions
+     is identical for all 4 buttons.
+     */
     $('.secret_meme1').click(function(e){
         e.preventDefault();
         if(counter1 == false){
